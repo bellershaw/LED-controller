7758aa3d1e097e20042ae304d198096588aea6b5
Braeden <braeden.ellershaw@gmail.com>
2025-04-11 21:23:11 -0700

    Formatting changes

--- lights-interface/src/App.js
+++ lights-interface/src/App.js
@@ -41,6 +41,28 @@ function App() {
         color_div.appendChild(color_display)
   } 
 
+  const breathe  = (color) => {
+    setHsva({ ...hsva, ...color.hsva })
+    console.log('color', color)
+    console.log(color.rgb)
+    console.log(color.g)
+
+    axios.put('http://192.168.0.2:8000/change_color', {},
+     { params : {
+      red : color.rgb.r,
+      green : color.rgb.g,
+      blue : color.rgb.b}}
+    )
+     .then(response => {
+      //Handle the response
+      console.log("ERROR ", + response);
+      })
+      .catch(error => {
+        console.error(error);
+      // Handle errors
+      });
+
+  }
   ;
   return (
     <div className="App">
@@ -53,6 +75,15 @@ function App() {
       <div className="Buttons">
         <button type="button" onClick={createColorDisplay} class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-1vh">Add</button>
         <button type="button" class="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-1vh">Remove</button>
+      </div>
+      <div className="Effects-Div">
+        Effects
+        <div className="Effects-Buttons">
+        <Button
+        title="Press me"
+        onPress={() => breathe}
+        />
+        </div>
       </div>
     </div>
   );
